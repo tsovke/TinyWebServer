62a30a647a6278f81168b1d729b76d0fa945a1d5
tsovke <tsovke@gmail.com>
2024-07-21 02:48:12 +0800

    init

---
+++ code/buffer/buffer.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <atomic>
+#include <cassert>
+#include <cstring>
+#include <iostream>
+#include <sys/uio.h>
+#include <unistd.h>
+#include <vector>
+
+using namespace std;
+
+class Buffer {};
