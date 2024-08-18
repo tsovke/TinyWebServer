d98e3391369a925d36dd3fe0e2b09e3576066ac4
tsovke <tsovke@gmail.com>
2024-08-18 23:27:09 +0800

    locker ok

--- code/locker/locker.hpp
+++ code/locker/locker.hpp
@@ -9,43 +9,65 @@
 // 互斥锁
 class locker {
 public:
-  locker(){
-    if (pthread_mutex_init(&m_mutex,nullptr)!=0) {
- throw std::exception();     
+  locker() {
+    if (pthread_mutex_init(&m_mutex, nullptr) != 0) {
+      throw std::exception();
     }
   }
-  ~locker(){
-    pthread_mutex_destroy(&m_mutex);
-  }
-  bool lock(){
-    return pthread_mutex_lock(&m_mutex);
-  }
-  bool unlock(){
-    return pthread_mutex_unlock(&m_mutex);
-  }
+  ~locker() { pthread_mutex_destroy(&m_mutex); }
+  bool lock() { return pthread_mutex_lock(&m_mutex); }
+  bool unlock() { return pthread_mutex_unlock(&m_mutex); }
+
 private:
   pthread_mutex_t m_mutex;
 };
 
 // 条件变量
-class cond{
-  public:
-  cond(){
-
+class cond {
+public:
+  cond() {
     if (pthread_cond_init(&m_cond, nullptr) != 0) {
       throw std::exception();
     }
   }
-  ~cond(){
-    
-    pthread_cond_destroy(&m_cond);
+
+  ~cond() { pthread_cond_destroy(&m_cond); }
+  bool wait(pthread_mutex_t *mutex) {
+    return pthread_cond_wait(&m_cond, mutex) == 0;
   }
-  bool wait(pthread_mutex_t *mutex){
-    return pthread_cond_wait(&m_cond,mutex)==0;
+
+  bool timewait(pthread_mutex_t *mutex, struct timespec t) {
+    return pthread_cond_timedwait(&m_cond, mutex, &t) == 0;
   }
-  timewait(){}
-  signal(){}
-  broadcast(){}
-  private:
-    pthread_cond_t m_cond;
+
+  bool signal() { return pthread_cond_signal(&m_cond); }
+
+  bool broadcast() { return pthread_cond_broadcast(&m_cond); }
+
+private:
+  pthread_cond_t m_cond;
+};
+
+// 信号量
+class sem {
+public:
+  sem() {
+    if (sem_init(&m_sem, 0, 0) != 0) {
+      throw std::exception();
+    }
+  }
+
+  sem(int num) {
+    if (sem_init(&m_sem, 0, num) != 0) {
+      throw std::exception();
+    }
+  }
+
+  ~sem() { sem_destroy(&m_sem); }
+
+  bool wait() { return sem_wait(&m_sem) == 0; }
+  bool post() { return sem_post(&m_sem) == 0; }
+
+private:
+  sem_t m_sem;
 };
