03c3485951b5ac1eddc548d0b195622d60e2cca6
tsovke <tsovke@gmail.com>
2024-08-31 10:31:13 +0800

    update

--- code/http/http_conn.cpp
+++ code/http/http_conn.cpp
@@ -195,7 +195,7 @@ http_conn::HTTP_CODE http_conn::parse_request_line(char *text) {
   if (!m_url || m_url[0] != '/') {
     return BAD_REQUEST;
   }
-// d
+// desgt
   return NO_REQUEST;
 }
 http_conn::HTTP_CODE http_conn::parse_headers(char *text) { return NO_REQUEST; }
